c8c835f9857c4c8ec0986c7d2df2b919ad037df7
ans_sharma <ak.sharma4918@gmail.com>
2022-06-30 23:30:50 +0530

    algo modified but same result

--- Assignment3.py
+++ Assignment3.py
@@ -19,42 +19,32 @@ START = 1
 MAXSIZE = 10000
 STEPS = 1000
 
+# heapify
 def heapify(arr, n, i):
-	largest = i # Initialize largest as root
-	l = 2 * i + 1	 # left = 2*i + 1
-	r = 2 * i + 2	 # right = 2*i + 2
-
-	# See if left child of root exists and is
-	# greater than root
-	if l < n and arr[largest] < arr[l]:
-		largest = l
-
-	# See if right child of root exists and is
-	# greater than root
-	if r < n and arr[largest] < arr[r]:
-		largest = r
-
-	# Change root, if needed
-	if largest != i:
-		arr[i], arr[largest] = arr[largest], arr[i] # swap
-
-		# Heapify the root.
-		heapify(arr, n, largest)
-
-# The main function to sort an array of given size
-
-
+   largest = i # largest value
+   l = 2 * i + 1 # left
+   r = 2 * i + 2 # right
+   # if left child exists
+   if l < n and arr[i] < arr[l]:
+      largest = l
+   # if right child exits
+   if r < n and arr[largest] < arr[r]:
+      largest = r
+   # root
+   if largest != i:
+      arr[i],arr[largest] = arr[largest],arr[i] # swap
+      # root.
+      heapify(arr, n, largest)
+# sort
 def heapSort(arr):
-	n = len(arr)
-
-	# Build a maxheap.
-	for i in range(n//2 - 1, -1, -1):
-		heapify(arr, n, i)
-
-	# One by one extract elements
-	for i in range(n-1, 0, -1):
-		arr[i], arr[0] = arr[0], arr[i] # swap
-		heapify(arr, i, 0)
+   n = len(arr)
+   # maxheap
+   for i in range(n, -1, -1):
+      heapify(arr, n, i)
+   # element extraction
+   for i in range(n-1, 0, -1):
+      arr[i], arr[0] = arr[0], arr[i] # swap
+      heapify(arr, i, 0)
 
 # Driver code
 # arr = [12, 11, 13, 5, 6, 7]
